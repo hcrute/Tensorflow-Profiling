8175f455fbd9984ceb2bd777dadc7b0308c2a8b7
Henry Crute <henrycrute@gmail.com>
2017-05-11 15:30:44 -0500

    updated

--- scripts/callgraph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /home/henry/Projects/fathom
-
-python -m cProfile -o alexnet.cprof ./fathom/alexnet/alexnet.py
-python -m cProfile -o deepq.cprof ./fathom/deepq/deepq.py
-python -m cProfile -o memnet.cprof ./fathom/memnet/memnet.py
-python -m cProfile -o residual.cprof ./fathom/residual/residual.py
-python -m cProfile -o seq2seq.cprof ./fathom/seq2seq/seq2seq.py
-python -m cProfile -o vgg.cprof ./fathom/vgg/vgg.py
